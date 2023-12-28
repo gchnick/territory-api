9478468602349db0103ae1b243d82f49b8ed3f8c
Nick Galan <gnick.alan@gmail.com>
2023-12-28 07:51:14 -0400

    feat(logger): :sparkles: add wran

--- src/shared/domain/logger.ts
+++ src/shared/domain/logger.ts
@@ -2,5 +2,6 @@ export default abstract class Logger {
   abstract debug(message: string): void;
   abstract error(message: string | Error): void;
   abstract info(message: string): void;
+  abstract warn(message: string): void;
   abstract setContext(context: string): void;
 }
