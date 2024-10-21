304346024688ce7394401158cd2210134c183c50
Nick Galán <gnick.alan@gmail.com>
2024-10-21 17:39:40 -0400

    refactor(shared): :recycle: add # to private fields

--- src/contexts/shared/domain/aggregate-root.ts
+++ src/contexts/shared/domain/aggregate-root.ts
@@ -1,21 +1,21 @@
 import { DomainEvent } from "./domain-event";
 
 export abstract class AggregateRoot {
-  private domainEvents: Array<DomainEvent>;
+  #domainEvents: Array<DomainEvent>;
 
   constructor() {
-    this.domainEvents = [];
+    this.#domainEvents = [];
   }
 
   pullDomainEvents(): Array<DomainEvent> {
-    const domainEvents = [...this.domainEvents];
-    this.domainEvents = [];
+    const domainEvents = [...this.#domainEvents];
+    this.#domainEvents = [];
 
     return domainEvents;
   }
 
   record(event: DomainEvent): void {
-    this.domainEvents.push(event);
+    this.#domainEvents.push(event);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
