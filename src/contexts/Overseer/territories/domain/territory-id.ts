c3cb8b847373ba723070ffeb15c57d46c84b4942
Nick Galán <gnick.alan@gmail.com>
2024-10-21 18:04:14 -0400

    fix(territory): :bug: temporal fix to object parameter in territory uuid

--- src/contexts/Overseer/territories/domain/territory-id.ts
+++ src/contexts/Overseer/territories/domain/territory-id.ts
@@ -1,3 +1,12 @@
 import { Uuid } from "@/shared/domain/value-object/uuid";
 
-export class TerritoryId extends Uuid {}
+export class TerritoryId extends Uuid {
+  // FIXME: The case of object parameter to Uuid no control here
+  constructor(value: string) {
+    let territoryIdParsed = value;
+    if (typeof value === "object") {
+      territoryIdParsed = (value as TerritoryId).value;
+    }
+    super(territoryIdParsed);
+  }
+}
