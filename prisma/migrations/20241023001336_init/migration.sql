bfed8759415a254f2f6e45771b12c664eea8ff0f
Nick Galán <gnick.alan@gmail.com>
2024-10-27 13:58:39 -0400

    refactor :recycle: add config and schema prisma

---
+++ prisma/migrations/20241023001336_init/migration.sql
@@ -0,0 +1,274 @@
+-- CreateTable
+CREATE TABLE "congregations" (
+    "number" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "circuit" TEXT NOT NULL,
+    "number_of_territories" INTEGER DEFAULT 0,
+    "map_image_url" TEXT,
+    "north_limit" TEXT NOT NULL,
+    "south_limit" TEXT NOT NULL,
+    "east_limit" TEXT NOT NULL,
+    "west_limit" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "territories" (
+    "teritory_id" TEXT NOT NULL PRIMARY KEY,
+    "number" INTEGER NOT NULL,
+    "label" TEXT NOT NULL,
+    "sector" TEXT NOT NULL,
+    "quantity_houses" INTEGER NOT NULL DEFAULT 0,
+    "locality" TEXT NOT NULL,
+    "locality_in_part" TEXT,
+    "map_image_url" TEXT,
+    "last_date_completed" DATETIME NOT NULL,
+    "current_assigned" BOOLEAN NOT NULL DEFAULT false,
+    "congregation_id" INTEGER NOT NULL,
+    CONSTRAINT "territories_congregation_id_fkey" FOREIGN KEY ("congregation_id") REFERENCES "congregations" ("number") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "meeting_places" (
+    "meeting_place_id" TEXT NOT NULL PRIMARY KEY,
+    "place" TEXT NOT NULL,
+    "mobile_phone" TEXT,
+    "field_service" BOOLEAN NOT NULL DEFAULT false,
+    "latitude" TEXT,
+    "longitude" TEXT,
+    "territory_id" TEXT NOT NULL,
+    CONSTRAINT "meeting_places_territory_id_fkey" FOREIGN KEY ("territory_id") REFERENCES "territories" ("teritory_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "meeting_place_availability" (
+    "day" TEXT NOT NULL,
+    "frequency" TEXT NOT NULL,
+    "moment" TEXT NOT NULL,
+    "meeting_place_id" TEXT NOT NULL,
+
+    PRIMARY KEY ("meeting_place_id", "day"),
+    CONSTRAINT "meeting_place_availability_meeting_place_id_fkey" FOREIGN KEY ("meeting_place_id") REFERENCES "meeting_places" ("meeting_place_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "periods" (
+    "period_id" TEXT NOT NULL PRIMARY KEY,
+    "description" TEXT NOT NULL,
+    "start_date" DATETIME NOT NULL,
+    "finish_date" DATETIME
+);
+
+-- CreateTable
+CREATE TABLE "registries" (
+    "registry_id" TEXT NOT NULL PRIMARY KEY,
+    "date_assigned" DATETIME NOT NULL,
+    "date_completed" DATETIME,
+    "conductor_id" TEXT NOT NULL,
+    "territory_id" TEXT NOT NULL,
+    "period_id" TEXT NOT NULL,
+    CONSTRAINT "registries_conductor_id_fkey" FOREIGN KEY ("conductor_id") REFERENCES "conductors" ("conductor_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "registries_territory_id_fkey" FOREIGN KEY ("territory_id") REFERENCES "territories" ("teritory_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "registries_period_id_fkey" FOREIGN KEY ("period_id") REFERENCES "periods" ("period_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "family_groups" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "label" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "phones" (
+    "phone_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "label" TEXT NOT NULL,
+    "number" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "roles" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "role" TEXT NOT NULL,
+    "description" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "user__role" (
+    "user_id" TEXT NOT NULL,
+    "role_id" INTEGER NOT NULL,
+
+    PRIMARY KEY ("user_id", "role_id"),
+    CONSTRAINT "user__role_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("user_id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "user__role_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "roles" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "user_id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "publisher_id" TEXT,
+    CONSTRAINT "users_publisher_id_fkey" FOREIGN KEY ("publisher_id") REFERENCES "publishers" ("publisher_id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "publishers" (
+    "publisher_id" TEXT NOT NULL PRIMARY KEY,
+    "householder" BOOLEAN NOT NULL DEFAULT false,
+    "sort_name" TEXT NOT NULL,
+    "firts_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "address" TEXT,
+    "mobile_phone" TEXT,
+    "service_group" INTEGER NOT NULL DEFAULT -1,
+    "photo_url" TEXT,
+    "privileges" TEXT NOT NULL,
+    "hope" TEXT NOT NULL,
+    "last_report" DATETIME,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "is_aparted" BOOLEAN NOT NULL DEFAULT false,
+    "email" TEXT,
+    "family_group_id" INTEGER NOT NULL,
+    CONSTRAINT "publishers_family_group_id_fkey" FOREIGN KEY ("family_group_id") REFERENCES "family_groups" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "publisher__phone" (
+    "publisher_id" TEXT NOT NULL,
+    "phone_id" INTEGER NOT NULL,
+
+    PRIMARY KEY ("publisher_id", "phone_id"),
+    CONSTRAINT "publisher__phone_publisher_id_fkey" FOREIGN KEY ("publisher_id") REFERENCES "publishers" ("publisher_id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "publisher__phone_phone_id_fkey" FOREIGN KEY ("phone_id") REFERENCES "phones" ("phone_id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "emergency_contacts" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "full_name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "publisher_id" TEXT NOT NULL,
+    CONSTRAINT "emergency_contacts_publisher_id_fkey" FOREIGN KEY ("publisher_id") REFERENCES "publishers" ("publisher_id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "elders" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "publisher_id" TEXT NOT NULL,
+    CONSTRAINT "elders_publisher_id_fkey" FOREIGN KEY ("publisher_id") REFERENCES "publishers" ("publisher_id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ministerial_servants" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "publisher_id" TEXT NOT NULL,
+    CONSTRAINT "ministerial_servants_publisher_id_fkey" FOREIGN KEY ("publisher_id") REFERENCES "publishers" ("publisher_id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "pionners" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "publisher_id" TEXT NOT NULL,
+    CONSTRAINT "pionners_publisher_id_fkey" FOREIGN KEY ("publisher_id") REFERENCES "publishers" ("publisher_id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "missionaries" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "publisher_id" TEXT NOT NULL,
+    CONSTRAINT "missionaries_publisher_id_fkey" FOREIGN KEY ("publisher_id") REFERENCES "publishers" ("publisher_id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "conductors" (
+    "conductor_id" TEXT NOT NULL PRIMARY KEY,
+    "last_date_assigned" DATETIME,
+    "publisher_id" TEXT NOT NULL,
+    CONSTRAINT "conductors_publisher_id_fkey" FOREIGN KEY ("publisher_id") REFERENCES "publishers" ("publisher_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "conductor_availability" (
+    "day" TEXT NOT NULL,
+    "frequency" TEXT NOT NULL,
+    "moment" TEXT NOT NULL,
+    "conductor_id" TEXT NOT NULL,
+
+    PRIMARY KEY ("conductor_id", "day"),
+    CONSTRAINT "conductor_availability_conductor_id_fkey" FOREIGN KEY ("conductor_id") REFERENCES "conductors" ("conductor_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "programs" (
+    "program_id" TEXT NOT NULL PRIMARY KEY,
+    "since_week" DATETIME NOT NULL,
+    "until_week" DATETIME NOT NULL,
+    "is_published" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "assignaments" (
+    "assignament_id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL,
+    "covered" BOOLEAN NOT NULL DEFAULT false,
+    "meeting_place_id" TEXT NOT NULL,
+    "coductor_id" TEXT NOT NULL,
+    "program_id" TEXT NOT NULL,
+    CONSTRAINT "assignaments_meeting_place_id_fkey" FOREIGN KEY ("meeting_place_id") REFERENCES "meeting_places" ("meeting_place_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "assignaments_coductor_id_fkey" FOREIGN KEY ("coductor_id") REFERENCES "conductors" ("conductor_id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "assignaments_program_id_fkey" FOREIGN KEY ("program_id") REFERENCES "programs" ("program_id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "congregations_name_key" ON "congregations"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "territories_number_key" ON "territories"("number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "territories_label_key" ON "territories"("label");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "phones_number_key" ON "phones"("number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "roles_role_key" ON "roles"("role");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_publisher_id_key" ON "users"("publisher_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "publishers_sort_name_key" ON "publishers"("sort_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "publishers_mobile_phone_key" ON "publishers"("mobile_phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "publishers_email_key" ON "publishers"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "publishers_firts_name_last_name_key" ON "publishers"("firts_name", "last_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "emergency_contacts_phone_key" ON "emergency_contacts"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "emergency_contacts_publisher_id_key" ON "emergency_contacts"("publisher_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "elders_publisher_id_key" ON "elders"("publisher_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ministerial_servants_publisher_id_key" ON "ministerial_servants"("publisher_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "pionners_publisher_id_key" ON "pionners"("publisher_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "missionaries_publisher_id_key" ON "missionaries"("publisher_id");
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { Module } from "@nestjs/common";
 import { ConditionalModule, ConfigModule } from "@nestjs/config";
 import { RouterModule } from "@nestjs/core";
 import { JwtModule } from "@nestjs/jwt";
-import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { TerritoryModule } from "@/app/overseer/territories/territory.module";
 import { AuthModule } from "@/app/shared/auth/auth.module";
 import { HealthModule } from "@/app/shared/health/health.module";
 import { UserModule } from "@/app/shared/user/user.module";
 
+import { PrismaModule } from "@/contexts/shared/infrastructure/persistence/prisma/prisma.module";
+
 import { CommandModule } from "@/core/command-bus/command.module";
 import configOptions from "@/core/config/config-options";
 import jwtAsyncOptions from "@/core/config/jwt-async-options";
 import routes from "@/core/config/routes";
-import typeOrmOptions from "@/core/config/typeorm/type-orm-options";
 import { EventBusModule } from "@/core/event-bus/event-bus.module";
 import { LoggerModule } from "@/core/logger/logger.module";
 import { QueryModule } from "@/core/query-bus/query.module";
@@ -29,9 +29,9 @@ import { SeedModule } from "@/core/seed/seed.module";
     CommandModule,
     QueryModule,
     EventBusModule,
+    PrismaModule,
     ConfigModule.forRoot(configOptions()),
     JwtModule.registerAsync(jwtAsyncOptions()),
-    TypeOrmModule.forRootAsync(typeOrmOptions()),
     RouterModule.register(routes()),
     ConditionalModule.registerWhen(SeedModule, "USE_SEEDS"),
   ],
