0bfae638f9b029360a9f24ac40f00c0021ee049c
Nick Galán <gnick.alan@gmail.com>
2025-02-02 07:41:58 -0400

    feat(user): :sparkles: remove name field from CreateUserCommand and update handler accordingly

--- src/contexts/shared/users/application/create/create-user-command-handler.ts
+++ src/contexts/shared/users/application/create/create-user-command-handler.ts
@@ -3,11 +3,11 @@ import { CommandHandler } from "@/shared/domain/command-handler";
 import { Injectable } from "@/shared/infrastructure/dependency-injection/injectable";
 
 import { CreateUserCommand } from "@/contexts/shared/users/application/create/create-user.command";
+import { RoleName } from "@/contexts/shared/users/domain/role/role-name";
 import { UserEmail } from "@/contexts/shared/users/domain/user-email";
 import { UserId } from "@/contexts/shared/users/domain/user-id";
 import { UserPassword } from "@/contexts/shared/users/domain/user-password";
 
-import { RoleName } from "../../domain/role/role-name";
 import { UserCreator } from "./user-creator";
 
 @Injectable()
