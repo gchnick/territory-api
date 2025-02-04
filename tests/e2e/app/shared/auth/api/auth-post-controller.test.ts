4c18ead36f989afea81d0046542d628af487f1e0
Nick Galán <gnick.alan@gmail.com>
2025-02-04 18:19:00 -0400

    test(auth): :test_tube: enhance e2e tests for login and signup with password encoding

--- tests/e2e/app/shared/auth/api/auth-post-controller.test.ts
+++ tests/e2e/app/shared/auth/api/auth-post-controller.test.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -10,10 +12,13 @@ import {
   prepareRolesInDB,
   prepareUsersInDB,
 } from "@/tests/e2e/app/shared/users/helper";
+import { UserPasswordMother } from "@/tests/unit/src/contexts/shared/users/domain/user-password-mother";
 
 import { AuthModule } from "@/app/shared/auth/auth.module";
 import { UserModule } from "@/app/shared/user/user.module";
 
+import { Encode } from "@/contexts/shared/auth/domain/encode";
+import { Role } from "@/contexts/shared/users/domain/role/role-name";
 import { User } from "@/contexts/shared/users/domain/user";
 import { UserRepository } from "@/contexts/shared/users/domain/user-repository";
 import { UserRole } from "@/contexts/shared/users/domain/user-role";
@@ -24,6 +29,7 @@ import { SignupPostRequestMother } from "../requests/signup-post-request-mother"
 describe("AuthPostController (e2e)", () => {
   let app: NestFastifyApplication;
   let repo: UserRepository;
+  let encode: Encode;
   let roles: UserRole[];
 
   beforeAll(async () => {
@@ -37,6 +43,7 @@ describe("AuthPostController (e2e)", () => {
     await app.init();
     await app.getHttpAdapter().getInstance().ready();
     repo = await app.get(UserRepository);
+    encode = await app.get(Encode);
     roles = await prepareRolesInDB(repo);
 
     nock.disableNetConnect();
@@ -54,13 +61,18 @@ describe("AuthPostController (e2e)", () => {
 
   describe("/v1/api/auth/login (POST)", () => {
     let users: Array<User>;
+    let password: string;
     beforeEach(async () => {
-      users = await prepareUsersInDB(repo, roles);
+      password = UserPasswordMother.create().value;
+      const passwordEncode = await encode.hash(
+        password,
+        User.SALT_OR_ROUNDS_ENCODE,
+      );
+      users = await prepareUsersInDB(repo, roles, passwordEncode);
     });
 
     it("should generate token with valid credentials", async () => {
       const email = users[0].email.value;
-      const password = users[0].password.value;
 
       const request = {
         email,
@@ -73,8 +85,10 @@ describe("AuthPostController (e2e)", () => {
         payload: request,
       });
 
+      const body = JSON.parse(response.body);
       expect(response.statusCode).toBe(200);
-      expect(response.body).toHaveProperty("access_token");
+      expect(body).toHaveProperty("access_token");
+      expect(typeof body.access_token).toBe("string");
     });
 
     it("should throw 401 with invalid credentials", async () => {
@@ -86,18 +100,20 @@ describe("AuthPostController (e2e)", () => {
         payload: request,
       });
 
+      const body = JSON.parse(response.body);
       expect(response.statusCode).toBe(401);
-      expect(response.body).toHaveProperty("message");
+      expect(body).toHaveProperty("message");
+      expect(typeof body.message).toBe("string");
     });
 
     it("should signup a new user", async () => {
       const request = SignupPostRequestMother.create({
-        roles: ["SERVICE_OVERSEER"],
+        roles: [Role.SERVICE_OVERSEER],
       });
 
       const response = await app.inject({
         method: "POST",
-        url: "/signup",
+        url: "/auth/signup",
         payload: request,
       });
 
