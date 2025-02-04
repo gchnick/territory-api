05216c63fce724c8aeaf196519bc248df4127368
Nick Galán <gnick.alan@gmail.com>
2025-02-04 00:23:43 -0400

    fix(user): :bug: remove name property from UserPostRequest for simplified request validation

--- src/app/shared/user/api/requests/user-post-request.ts
+++ src/app/shared/user/api/requests/user-post-request.ts
@@ -1,10 +1,6 @@
 import { IsArray, IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class UserPostRequest {
-  @IsString()
-  @IsNotEmpty()
-  name!: string;
-
   @IsEmail()
   email!: string;
 
