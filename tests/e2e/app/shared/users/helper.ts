5c6188872cd3348602905c15450ec765c3dfbd15
Nick Galán <gnick.alan@gmail.com>
2025-02-04 00:14:11 -0400

    feat(tests): :sparkles: refactor role and user preparation functions for improved database setup

--- tests/e2e/app/shared/users/helper.ts
+++ tests/e2e/app/shared/users/helper.ts
@@ -6,28 +6,45 @@ import { User } from "@/contexts/shared/users/domain/user";
 import { UserRepository } from "@/contexts/shared/users/domain/user-repository";
 import { UserRole } from "@/contexts/shared/users/domain/user-role";
 
-export const createAllRoles = (): UserRole[] =>
+const createAllRoles = (): UserRole[] =>
   Object.keys(Role).map((role, index) =>
     UserRoleMother.create({ id: index, name: RoleName.fromValue(role).value }),
   );
 
-export const createUsers = (roles: UserRole[]): User[] => {
-  const overseer = roles.find(r => r.name.value === Role.SERVICE_OVERSEER);
-  return overseer
-    ? [UserMother.create({ roles: [overseer.toPrimitives()] })]
-    : [];
-};
-
-export const saveInitialRoles = async (
+const saveInitialRoles = async (
   repo: UserRepository,
   roles: UserRole[],
 ): Promise<void[]> => {
   return Promise.all(roles.map(role => repo.saveRole(role)));
 };
 
-export const saveInitialUsers = async (
+export const prepareRolesInDB = async (repo: UserRepository) => {
+  const roles = createAllRoles();
+  await repo.deleteAllRoles();
+  await saveInitialRoles(repo, roles);
+  return roles;
+};
+
+const createUsers = (roles: UserRole[]): User[] => {
+  const overseer = roles.find(r => r.name.value === Role.SERVICE_OVERSEER);
+  return overseer
+    ? [UserMother.create({ roles: [overseer.toPrimitives()] })]
+    : [];
+};
+
+const saveInitialUsers = async (
   repo: UserRepository,
   users: User[],
 ): Promise<void[]> => {
   return Promise.all(users.map(user => repo.save(user)));
+};
+
+export const prepareUsersInDB = async (
+  repo: UserRepository,
+  roles: UserRole[],
+) => {
+  await repo.deleteAll();
+  const users = createUsers(roles);
+  await saveInitialUsers(repo, users);
+  return users;
 };
