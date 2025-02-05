d82fccfc6c5b7f3ddee59e4c0d49ef759cb05247
Nick Galán <gnick.alan@gmail.com>
2025-02-04 21:31:23 -0400

    refactor(tests): :recycle: streamline territory test setup by using prepareTerritoriesInDB helper

--- tests/e2e/app/overseer/territories/api/territory-get.controller.test.ts
+++ tests/e2e/app/overseer/territories/api/territory-get.controller.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import {
   FastifyAdapter,
   NestFastifyApplication,
@@ -6,9 +7,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import * as nock from "nock";
 
 import { baseTestModuleImports } from "@/tests/e2e/app/helpers/base-test-module-imports";
-import { saveInitialTerritories } from "@/tests/e2e/app/overseer/territories/helpers";
-import { CongregationMother } from "@/tests/unit/src/contexts/Overseer/congregation/domain/congregation-mother";
-import { TerritoryMother } from "@/tests/unit/src/contexts/Overseer/territories/domain/territory-mother";
+import { prepareTerritoriesInDB } from "@/tests/e2e/app/overseer/territories/helpers";
 
 import { CongregationModule } from "@/app/overseer/congregations/congregation.module";
 import { TerritoryModule } from "@/app/overseer/territories/territory.module";
@@ -19,7 +18,6 @@ import { Territory } from "@/contexts/Overseer/territories/domain/territory";
 import { TerritoryRepository } from "@/contexts/Overseer/territories/domain/territory-repository";
 
 describe("TerritoryGetController (e2e)", () => {
-  const LENGHT_INITIAL_TERRITORY = 20;
   let app: NestFastifyApplication;
   let congregationRepo: CongregationRepository;
   let territoryRepo: TerritoryRepository;
@@ -57,15 +55,12 @@ describe("TerritoryGetController (e2e)", () => {
     let congregation: Congregation;
     let territories: Array<Territory>;
     beforeEach(async () => {
-      await congregationRepo.deleteAll();
-      await territoryRepo.deleteAll();
-      congregation = CongregationMother.create();
-      territories = TerritoryMother.createSuccession(
-        LENGHT_INITIAL_TERRITORY,
-        congregation.number.value,
+      const result = await prepareTerritoriesInDB(
+        congregationRepo,
+        territoryRepo,
       );
-      await congregationRepo.save(congregation);
-      await saveInitialTerritories(territoryRepo, territories);
+      congregation = result.congregation;
+      territories = result.territories;
     });
 
     it("should fetch all territories", async () => {
@@ -81,7 +76,7 @@ describe("TerritoryGetController (e2e)", () => {
 
     it("should find territory by number and congregation", async () => {
       const congregationNumber = congregation.number.value;
-      const numberParam = 10;
+      const numberParam = 2;
       const expectedLabel = territories[numberParam - 1].label.value;
 
       const response = await app.inject({
@@ -105,13 +100,12 @@ describe("TerritoryGetController (e2e)", () => {
         url:
           "/territories?" +
           `filters[0][field]=congregation&filters[0][operator]=EQUAL&filters[0][value]=${congregationNumber}` +
-          "filters[1][field]=isAssigned&filters[1][operator]=EQUAL&filters[1][value]=false" +
+          "&filters[1][field]=isAssigned&filters[1][operator]=EQUAL&filters[1][value]=false" +
           "&orderBy=lastCompleted&order=ASC",
       });
 
       expect(response.statusCode).toBe(200);
       expect(response.headers["content-type"]).toMatch(/application\/json/);
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       expect(JSON.parse(response.payload).data).toHaveLength(expectedLength);
     });
   });
