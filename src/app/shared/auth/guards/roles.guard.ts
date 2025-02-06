1bfbd3c592c6a227e068021e7af00c50ed6b96df
Nick Galán <gnick.alan@gmail.com>
2025-02-06 07:53:52 -0400

    refactor(auth): :recycle: improve RolesGuard to use optional chaining and enhance readability

--- src/app/shared/auth/guards/roles.guard.ts
+++ src/app/shared/auth/guards/roles.guard.ts
@@ -8,17 +8,17 @@ import { ROLES_KEY } from "../decorators/roles.decorator";
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(private readonly _reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles = this._reflector.getAllAndOverride<Role[] | undefined>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
     if (!requiredRoles) {
       return true;
     }
-    const { user } = context.switchToHttp().getRequest<{ user: JwtPayload }>();
-    return requiredRoles.some(role => user.roles?.includes(role));
+    const { user } = context.switchToHttp().getRequest<{ user?: JwtPayload }>();
+    return requiredRoles.some(role => user?.roles.includes(role));
   }
 }
