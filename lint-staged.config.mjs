82dba2a4307c08a0694ea7aa092dc3161cd7105c
Nick Galán <gnick.alan@gmail.com>
2025-02-05 20:26:06 -0400

    chore:  :wrench: update ESLint and YAML linting configurations, enhance lint-staged commands

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,12 @@
 const config = {
   "**/*.{ts?(x),mts}": () => "tsc -p tsconfig.prod.json --noEmit",
-  "*.{js,jsx,mjs,cjs,ts,tsx,mts}": ["npm run lint", "vitest related --run"],
+  "*.{js,jsx,mjs,cjs,ts,tsx,mts}": [
+    "node --run lint:file",
+    "vitest related --run",
+  ],
   "*.{md,json}": "prettier --write",
-  "*": "npm run typos",
-  "*.{yml,yaml}": "npm run lint:yaml",
+  "*": "node --run typos",
+  "*.{yml,yaml}": "node --run lint:yaml",
 };
 
 export default config;
