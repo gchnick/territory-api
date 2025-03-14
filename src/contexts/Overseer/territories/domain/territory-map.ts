0090f6451439497b1647b5dd971c80b3c8c683bc
Nick Galán <gnick.alan@gmail.com>
2025-03-14 18:58:59 -0400

    refactor(territory): :recycle: update territory map

--- src/contexts/Overseer/territories/domain/territory-map.ts
+++ src/contexts/Overseer/territories/domain/territory-map.ts
@@ -1,29 +1,3 @@
-import { StringValueObject } from "@/shared/domain/value-object/string-value-object";
+import { UrlValueObject } from "@/contexts/shared/domain/url-value-object";
 
-import { TerritoryMapIsEmpty } from "./territory-map-is-empty";
-import { TerritoryMapLengthExceeded } from "./territory-map-length-exceeded";
-
-export class TerritoryMap extends StringValueObject {
-  static MAXIMUM_CHARACTERS = 255;
-
-  constructor(value: string) {
-    super(value);
-    this.#ensureIsNotEmpty(value);
-    this.#ensureLengthIsLessThanMaximumCharacters(value);
-  }
-
-  #ensureIsNotEmpty(value: string) {
-    if (value.trim().length === 0) {
-      throw new TerritoryMapIsEmpty(`The Territory Map is empty`);
-    }
-  }
-
-  #ensureLengthIsLessThanMaximumCharacters(value: string) {
-    const maximum = TerritoryMap.MAXIMUM_CHARACTERS;
-    if (value.length > maximum) {
-      throw new TerritoryMapLengthExceeded(
-        `The Territory Map <${value}> has more than <${maximum}> characters`,
-      );
-    }
-  }
-}
+export class TerritoryMap extends UrlValueObject {}
